1da5659f1dbafc3dd170420689d6a9239759d996
ahmed0soliman6 <301318054+ahmed0soliman6@users.noreply.github.com>
2026-08-17 22:32:05 +0000

    Fix release token handling and publish v1.3.24

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -3,7 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('MitaliDesktop', Object.freeze({
   isDesktop: true,
   platform: process.platform,
-  appVersion: 'v1.3.23',
+  appVersion: 'v1.3.24',
   getUpdateState: () => ipcRenderer.invoke('update:get-state'),
   checkForUpdate: () => ipcRenderer.invoke('update:check'),
   downloadUpdate: () => ipcRenderer.invoke('update:download'),
